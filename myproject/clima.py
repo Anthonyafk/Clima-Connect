a05833fab9be1e87de2b8fb46878b7fa2fecf007
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2023-09-09 22:33:37 -0600

    Cambios en clima.py de la app v1.4
    Se borraron dos imports

--- myproject/clima.py
+++ myproject/clima.py
@@ -1,68 +1,26 @@
+# Imports
 from flask import Flask, render_template, request
-import requests
-import difflib
+from API.getWeather import obtener_datos_del_tiempo, encontrar_nombre_similar
 
+
+# para runnear la app
 app = Flask(__name__)
 
-# Tu clave de API de OpenWeatherMap (reemplaza 'TU_API_KEY' con tu clave)
-api_key = '36a510e66cf5393e1e5e2e70c2aad861'
-
-# Lista de nombres conocidos y sus IATA code en minúsculas
-nombres_abreviados = {
-    "mexico city": ["ciudad de méxico", "mex", "cdmx"],
-    "guadalajara": ["guadalajara", "gdl"],
-    "cancun": ["cancun", "cun"],
-    "tijuana": ["tijuana", "tij"],
-    "puerto vallarta": ["puerto vallarta", "pvr"],
-    "monterrey": ["monterrey", "mty"]
-}
-
-# Función para encontrar el nombre más cercano
-def encontrar_nombre_similar(entrada_usuario):
-    entrada_usuario = entrada_usuario.lower()  # Convertir a minúsculas
-    
-    for nombre, abreviaturas in nombres_abreviados.items():
-        if entrada_usuario in abreviaturas:
-            return nombre
-    
-    coincidencias = difflib.get_close_matches(entrada_usuario, nombres_abreviados.keys())
-    
-    if coincidencias:
-        return coincidencias[0]
-    else:
-        return None
-
 @app.route("/", methods=["GET", "POST"])
-def obtener_datos_del_tiempo():
-    ciudad = "Tokyo"  # Ciudad predeterminada
-    pais = "jp" # Pais por defecto
-    
+def main():
     if request.method == "POST":
-        entrada_ciudad = request.form.get("ciudad")
-        entrada_pais = request.form.get("pais")
-        
-        # Utiliza la función para encontrar el nombre similar
-        nombre_similar = encontrar_nombre_similar(entrada_ciudad)
-        
-        if nombre_similar:
-            ciudad = nombre_similar
-            pais = entrada_pais
-    
-    # URL de la API de OpenWeatherMap con ciudad y país
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad},{pais}&appid={api_key}"
+        ciudad = request.form.get("ciudad")  # Obtener la ciudad desde el formulario web
 
-    try:
-        response = requests.get(url)
-        data = response.json()
+        # Corregir el nombre de la ciudad si es necesario
+        ciudad_corregida = encontrar_nombre_similar(ciudad)
         
-        # Realiza la conversión de grados Kelvin a grados Celsius
-        temperatura_celsius = data.get("main", {}).get("temp", 0) - 273.15
-        
-        # Muestra los datos del tiempo en la página
-        return render_template("index.html", data=data, ciudad=ciudad, temperatura_celsius=temperatura_celsius)
-    except requests.exceptions.RequestException as e:
-        return f"Error al obtener datos del tiempo: {e}"
+        ciudad = ciudad_corregida  # Establecer la ciudad corregida como la ciudad de búsqueda
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        # Llama a obtener_datos_del_tiempo() y almacena la ciudad obtenida
+        ciudad = obtener_datos_del_tiempo(ciudad)
+
+        return f"{ciudad}"  # Mostrar la ciudad en la página web
+
+    return render_template("index.html")
+
 
