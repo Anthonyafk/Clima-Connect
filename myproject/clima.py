26368391f7d9b04bfc9c8cf6254be0ab4819035d
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2023-09-23 23:49:33 -0600

    Update clima.py
    Documentacion

--- myproject/clima.py
+++ myproject/clima.py
@@ -1,13 +1,20 @@
-# Imports
+"""
+Imports
+Importamos las bibliotecas necesarias para nuestra aplicación Flask.
+"""
 from flask import Flask, render_template, request
 from API.getWeather import obtener_datos_del_tiempo
 
-
-# para runnear la app
+# Inicialización de la aplicación Flask
 app = Flask(__name__)
 
+# Definición de la ruta principal ("/") y métodos permitidos (GET y POST)
 @app.route("/", methods=["GET", "POST"])
 def main():
+    """
+    Función principal de la aplicación Flask.
+    Se encarga de manejar las solicitudes GET y POST a la ruta principal ("/").
+    """
     if request.method == "POST":
         ciudad = request.form.get("ciudad")  # Obtener la ciudad desde el formulario web
         ciudad = obtener_datos_del_tiempo(ciudad)
@@ -15,3 +22,5 @@ def main():
         return f"{ciudad}"  # Mostrar la ciudad en la página web
 
     return render_template("index.html")
+
+
