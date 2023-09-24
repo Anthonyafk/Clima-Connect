84a488000907556ed089d895a9c4ab8faee0888f
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2023-09-23 18:08:37 -0600

    Correji el algoritmo de "encontrar_nombre_similar"

---
+++ myproject/utils/SintaxisErrorsbyUser.py
@@ -0,0 +1,50 @@
+import difflib
+
+# Lista de nombres conocidos y sus IATA code en minúsculas
+nombres_abreviados = {
+    "ciudad de méxico": ["ciudad de méxico", "mex", "cdmx", "mxm"],
+    "guadalajara": ["guadalajara", "gdl", "miguel hidalgo y costilla"],
+    "cancun": ["cancun", "cun"],
+    "acapulco": ["acapulco", "aca", "pulc"],
+    "aguascalientes": ["aguascalientes", "agu"],
+    "guanajuato": ["guanajuato", "bjx"],
+    "ciudad juárez": ["ciudad juárez", "cjs", "juá"],
+    "ciudad del carmen": ["ciudad del carmen", "cme", "car"],
+    "chetumal": ["chetumal", "ctm"],
+    "chihuahua": ["chihuahua", "cuu"],
+    "cozumel": ["cozumel", "czm", "ozu"],
+    "hermosillo": ["hermosillo", "hmo", "rmo"],
+    "santa maría huatulco": ["huatulco", "hux", "santa maría huatulco"],
+    "mérida": ["mérida", "mid"],
+    "oaxaca": ["oaxaca", "oax", "axa"],
+    "puebla": ["puebla", "pbc", "ebla"],
+    "puerto vallarta": ["puerto vallarta", "pvr", "licenciado gustavo díaz ordaz"],
+    "puerto escondido": ["puerto escondido", "pxm"],
+    "querétaro": ["querétaro", "qro", "eré"],
+    "san luis potosí": ["san luis potosí", "slp", "otosí"],
+    "tampico": ["tampico", "tam"],
+    "toluca": ["toluca", "tlc", "ait"],
+    "torreón": ["francisco sarabia", "trc", "torreón"],
+    "villahermosa": ["carlos rovirosa pérez", "vsa", "villahermosa"],
+    "zacatecas": ["zacatecas", "zcl"],
+    "ixtapa-zihuatanejo": ["zihuatanejo", "zih"],
+    "zihuatanejo": ["zihuatanejo", "zih"],
+    "tijuana": ["tijuana", "tij"],
+    "veracruz": ["veracruz", "ver", "acruz"],
+    "monterrey": ["monterrey", "mty", "mon"]
+}
+
+def encontrar_nombre_similar(entrada_usuario):
+    entrada_usuario = entrada_usuario.lower()
+    
+    for nombre, abreviaturas in nombres_abreviados.items():
+        if entrada_usuario in abreviaturas:
+            return nombre
+    
+    coincidencias = difflib.get_close_matches(entrada_usuario, nombres_abreviados.keys())
+    
+    if coincidencias:
+        return coincidencias[0]
+    else:
+        return None
+
