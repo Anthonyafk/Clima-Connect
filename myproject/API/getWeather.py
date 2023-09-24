589c050b5c8df12520a6fec9014163e55961d6c8
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2023-09-23 18:11:36 -0600

    Update getWeather.py
    Quite el algoritmo de encontra_nombre_similar y añadí la funcion para reconocer si se introduce un ticket o no

--- myproject/API/getWeather.py
+++ myproject/API/getWeather.py
@@ -1,44 +1,28 @@
 from flask import Flask, render_template, request
 from API.config import OPENWEATHER_API_KEY
 import requests
-import difflib
+from utils.SintaxisErrorsbyUser import encontrar_nombre_similar
+from Data.data_loader import get_origin_by_ticket, get_destination_by_ticket
 
 app = Flask(__name__)
 
 api_key = OPENWEATHER_API_KEY
 
-nombres_abreviados = {
-    "ciudad de méxico": ["ciudad de méxico", "mex", "cdmx", "mxm"],
-    "guadalajara": ["guadalajara", "gdl"],
-    "cancun": ["cancun", "cun"],
-    "tijuana": ["tijuana", "tij"],
-    "puerto vallarta": ["puerto vallarta", "pvr"],
-    "monterrey": ["monterrey", "mty"]
-    "veracruz": ["veracruz", "ver", "acruz"],
-    "monterrey": ["monterrey", "mty", "mon"]
-}
+def contiene_numeros(texto):
+    return any(char.isdigit() for char in texto)
 
-def encontrar_nombre_similar(entrada_usuario):
-    entrada_usuario = entrada_usuario.lower()
-    
-    for nombre, abreviaturas in nombres_abreviados.items():
-        if entrada_usuario in abreviaturas:
-            return nombre
-    
-    coincidencias = difflib.get_close_matches(entrada_usuario, nombres_abreviados.keys())
-    
-    if coincidencias:
-        return coincidencias[0]
-    else:
-        return None
-
-def obtener_datos_del_tiempo(ciudad="Tokyo" ):
-    pais = "jp"
+def obtener_datos_del_tiempo(ciudad="Cuernavaca" ):
+    pais = "mx"
     
     if request.method == "POST":
-        ciudad = request.form.get("ciudad")
-        pais = request.form.get("pais")
-        ciudad = encontrar_nombre_similar(ciudad)
+        entrada = request.form.get("ciudad")
+        
+        # Verifica si la entrada contiene números, si es un ticket.
+        if contiene_numeros(entrada):
+            ciudad_abreviada = get_destination_by_ticket(entrada)
+            ciudad = encontrar_nombre_similar(ciudad_abreviada)
+        else:
+           ciudad = encontrar_nombre_similar(entrada)
     
     url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad},{pais}&appid={api_key}"
 
@@ -55,4 +39,5 @@ def obtener_datos_del_tiempo(ciudad="Tokyo" ):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
