67990b957d97e27959dab87551a21d764af46fb5
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2023-09-23 18:13:50 -0600

    Update data_loader.py
    Se añaden los medos para extraer el IATA code de origen y destino

--- myproject/Data/data_loader.py
+++ myproject/Data/data_loader.py
@@ -1,72 +1,33 @@
 import csv
 
-# Cargar el DataSet 1 en un diccionario (usaremos las coordenadas como clave)
-dataset1 = {}
-with open('dataset1.csv', mode='r') as file:
+# Obtén la ruta completa al archivo CSV
+csv_file_path = r'C:\Users\danie\Downloads\Proyecto-1\myproject\Data\dataset2.csv'
+
+# Cargar los datos del archivo CSV en un diccionario
+data = {}
+with open(csv_file_path, mode='r') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        coords = (
-            float(row['origin_latitude']),
-            float(row['origin_longitude']),
-            float(row['destination_latitude']),
-            float(row['destination_longitude'])
-        )
-        dataset1[coords] = row
+        data[row['num_ticket']] = {
+            'origin': row['origin'],
+            'destination': row['destination'],
+            'origin_latitude': float(row['origin_latitude']),
+            'origin_longitude': float(row['origin_longitude']),
+            'destination_latitude': float(row['destination_latitude']),
+            'destination_longitude': float(row['destination_longitude'])
+        }
 
-# Cargar el DataSet 2 en un diccionario (usaremos el número de boleto como clave)
-dataset2 = {}
-with open('dataset2.csv', mode='r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        ticket_number = row['num_ticket']
-        dataset2[ticket_number] = row
-
-# Función para buscar por número de boleto
-def buscar_por_numero_de_boleto(numero_boleto):
-    if numero_boleto in dataset2:
-        return dataset2[numero_boleto]
+# Función para obtener el origen dado un num_ticket
+def get_origin_by_ticket(num_ticket):
+    if num_ticket in data:
+        return data[num_ticket]['origin']
     else:
-        return None  # El número de boleto no se encontró en el DataSet 2
+        return None  # Devuelve None si el num_ticket no se encuentra en los datos
 
-# Función para buscar por coordenadas
-def buscar_por_coordenadas(latitud_origen, longitud_origen, latitud_destino, longitud_destino):
-    coords = (latitud_origen, longitud_origen, latitud_destino, longitud_destino)
-    if coords in dataset1:
-        return dataset1[coords]
+# Función para obtener el destino dado un num_ticket
+def get_destination_by_ticket(num_ticket):
+    if num_ticket in data:
+        return data[num_ticket]['destination']
     else:
-        return None  # Las coordenadas no se encontraron en el DataSet 1
+        return None  # Devuelve None si el num_ticket no se encuentra en los datos
 
-# Crear un diccionario para mapear códigos IATA a ciudades
-iata_a_ciudad = {
-    "TLC": "Toluca",
-    "MTY": "Monterrey",
-    # Agrega otros mapeos de códigos IATA a ciudades aquí
-}
-
-# Función para obtener la ciudad de origen desde un código IATA
-def obtener_ciudad_desde_iata(origin_iata):
-    return origin_iata.get(origin_iata, "Ciudad Desconocida")  # Devuelve "Ciudad Desconocida" si no se encuentra el código IATA
-
-# Ejemplo de uso:
-numero_boleto = 'kw9f0kwvZJmsukQy'  # Cambia esto al número de boleto que deseas buscar
-resultado_boleto = buscar_por_numero_de_boleto(numero_boleto)
-
-if resultado_boleto:
-    print(f"Resultado de búsqueda por número de boleto ({numero_boleto}):")
-    print(resultado_boleto)
-else:
-    print(f"No se encontró ningún boleto con el número {numero_boleto}")
-
-# Ejemplo de búsqueda por coordenadas (cambia las coordenadas según tus necesidades):
-lat_origen = 19.3371
-lon_origen = -99.566 
-lat_destino = 25.7785
-lon_destino = -100.107
-
-resultado_coordenadas = buscar_por_coordenadas(lat_origen, lon_origen, lat_destino, lon_destino)
-
-if resultado_coordenadas:
-    print("\nResultado de búsqueda por coordenadas:")
-    print(resultado_coordenadas)
-else:
-    print("\nNo se encontraron coincidencias para las coordenadas proporcionadas")
