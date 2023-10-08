484e0372d9610080aab67c2253a8176daae8c416
Anthonyafk <111199295+Anthonyafk@users.noreply.github.com>
2023-10-08 17:04:01 -0600

    Se muestran imagenes >:)

--- myproject/API/getWeather.py
+++ myproject/API/getWeather.py
@@ -45,49 +45,63 @@ def obtener_datos_del_tiempo(ciudad="Cuernavaca"):
     ciudad_destino = ""  # Inicializa ciudad_destino con una cadena vacía
     pais = request.form.get("pais")
 
-    
-    
     if request.method == "POST":
         entrada = request.form.get("ciudad")
- 
-        
+
         # Verifica si la entrada contiene números, si es un ticket.
-    if contiene_numeros(entrada):
+        if contiene_numeros(entrada):
             ciudad_abreviada = obtener_ticket_de_destino(entrada)
             ciudad = encontrar_nombre_similar(ciudad_abreviada)
             origen = obtener_ticket_de_origen(entrada)
             ciudad_origen = encontrar_nombre_similar(origen)  # Obtiene la ciudad de origen
             ciudad_destino = encontrar_nombre_similar(origen)  # Asigna el mismo valor a ciudad_destino si es apropiado
-    else:
-           ciudad = encontrar_nombre_similar(entrada)
-           
-    if ciudad is None:
+        else:
+            ciudad = encontrar_nombre_similar(entrada)
+
+        if ciudad is None:
             error_message = "Nombre de ciudad o ticket inválido. Recargue la página e inténtelo otra vez."
             return render_template("error.html", error_message=error_message)
-    
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad},{pais}&appid={api_key}"
-    url_origen = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad_origen},{pais}&appid={api_key}"  # Obtén el clima de la ciudad de origen
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        
-        data = response.json()
-        temperatura_celsius = data.get("main", {}).get("temp", 0) - 273.15
-        
-        # Obtiene los datos del clima de la ciudad de origen
-        response_origen = requests.get(url_origen)
-        response_origen.raise_for_status()
-        data_origen = response_origen.json()
-        temperatura_celsius_origen = data_origen.get("main", {}).get("temp", 0) - 273.15
-        humedad_origen = data_origen.get("main", {}).get("humidity", 0)
-        descripcion_origen = data_origen['weather'][0]['description']
-        presion_origen = data_origen.get("main", {}).get("pressure", 0)
-        
-        return render_template("index.html", data=data, ciudad=ciudad, temperatura_celsius=temperatura_celsius, origen=origen, temperatura_celsius_origen=temperatura_celsius_origen, humedad_origen=humedad_origen, descripcion_origen=descripcion_origen, presion_origen=presion_origen, ciudad_destino=ciudad_destino, encontrar_nombre_similar=encontrar_nombre_similar)
-    except requests.exceptions.RequestException as e:
-        error_message = "Error al obtener datos del tiempo. Por favor, verifica tu conexión a Internet."
-        return render_template("error.html", error_message=error_message)
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad},{pais}&appid={api_key}"
+        url_origen = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad_origen},{pais}&appid={api_key}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+            temperatura_celsius = data.get("main", {}).get("temp", 0) - 273.15
+
+            # Obtén la descripción del clima
+            descripcion_clima = data['weather'][0]['description']
+
+            # Obtiene los datos del clima de la ciudad de origen
+            response_origen = requests.get(url_origen)
+            response_origen.raise_for_status()
+            data_origen = response_origen.json()
+            temperatura_celsius_origen = data_origen.get("main", {}).get("temp", 0) - 273.15
+            humedad_origen = data_origen.get("main", {}).get("humidity", 0)
+            descripcion_origen = data_origen['weather'][0]['description']
+            presion_origen = data_origen.get("main", {}).get("pressure", 0)
+
+            return render_template(
+                "index.html",
+                data=data,
+                ciudad=ciudad,
+                temperatura_celsius=temperatura_celsius,
+                origen=origen,
+                temperatura_celsius_origen=temperatura_celsius_origen,
+                humedad_origen=humedad_origen,
+                descripcion_origen=descripcion_origen,
+                presion_origen=presion_origen,
+                ciudad_destino=ciudad_destino,
+                encontrar_nombre_similar=encontrar_nombre_similar,
+                descripcion_clima=descripcion_clima  # Agrega la descripción del clima
+            )
+        except requests.exceptions.RequestException as e:
+            error_message = "Error al obtener datos del tiempo. Por favor, verifica tu conexión a Internet."
+            return render_template("error.html", error_message=error_message)
+
 
 # Nueva ruta para manejar la solicitud AJAX de obtención de datos climáticos
 @app.route("/obtener_datos_climaticos", methods=["POST"])
